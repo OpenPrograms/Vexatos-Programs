8b419954a496ec4c82d7b881b916bad3c03c4f89
Vexatos <Vexatos@users.noreply.github.com>
2014-05-03 17:00:50 +0200

    Update song.lua

--- song/song.lua
+++ song/song.lua
@@ -1,4 +1,4 @@
---[[Runs notes inside a table using special syntax, the Note API and computer.beep
+--[[Runs notes inside a table or a string (notes seperated by free spaces, then) using special syntax, the Note API and computer.beep
   Examples:
   "E5": Plays the note "E5" for the duration specified in the second parameter (default 0.125, equaling 120 bpm)
   "-E5": Plays the note "E5" with double the specified duration
@@ -10,28 +10,47 @@
 local n = require("note")
 local song = {}
 function song.play(notes,shortest)
-  if not type(notes) == "table" then
-    error("Wrong input given, song.play requires a table as first parameter",2)
-  end
   if not type(shortest)=="number" then
     shortest = 0.125
   end
   if not shortest then shortest = 0.125 end
-  local duration
-  for i,j in ipairs(notes) do
-    if string.find(j,"P") then
-      os.sleep(shortest*tonumber(string.match(j,"P_(%d+)")))
-      duration = 0
-    elseif string.find(j,"%-") then
-      duration = 2
-    elseif string.find(j,"_") then
-      duration = tonumber(string.match(j,".*_(%d+)"))
-    else
-      duration = 1
+  local typo = type(notes)
+  if typo == "table" then
+    local duration
+    for i,j in ipairs(notes) do
+      if string.find(j,"P") then
+        os.sleep(shortest*tonumber(string.match(j,"P_(%d+)")))
+        duration = 0
+      elseif string.find(j,"%-") then
+        duration = 2
+      elseif string.find(j,"_") then
+        duration = tonumber(string.match(j,".*_(%d+)"))
+      else
+        duration = 1
+      end
+      if duration ~= 0 then
+        n.play(string.match(j,"(%a.?%d)_?%d*"),shortest*duration)
+      end
     end
-    if duration ~= 0 then
-      n.play(string.match(j,"(%a.?%d)_?%d*"),shortest*duration)
+  elseif typo == "string" then
+    local duration
+    for j in string.gmatch(notes,"%S+") do
+      if string.find(j,"P") then
+        os.sleep(shortest*tonumber(string.match(j,"P_(%d+)")))
+        duration = 0
+      elseif string.find(j,"%-") then
+        duration = 2
+      elseif string.find(j,"_") then
+        duration = tonumber(string.match(j,".*_(%d+)"))
+      else
+        duration = 1
+      end
+      if duration ~= 0 then
+        n.play(string.match(j,"(%a.?%d)_?%d*"),shortest*duration)
+      end
     end
+  else
+    error("Wrong input given, song.play requires a table as first parameter",2)
   end
 end
 
