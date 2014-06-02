1aaf82cd32ddde6a3e30db5f0fa0e82772ef3a3a
Vexatos <Stuarzt@gmx.de>
2014-06-02 06:59:01 +0200

    Added experimental file.

---
+++ programs.lua
@@ -0,0 +1,12 @@
+{
+  ["song"] = {
+    files = {
+      ["master/song"] = "/"
+    },
+    dependencies = {
+	},
+        name = "Song API",
+        description = "An API to play whole songs using computer.beep",
+        author = "Vexatos"
+  }
+}
