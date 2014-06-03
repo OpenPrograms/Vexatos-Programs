76a5229999cce61f1dde3a2df07fce4330be4052
Vexatos <Vexatos@users.noreply.github.com>
2014-06-03 08:02:32 +0200

    Update programs.lua

--- programs.lua
+++ programs.lua
@@ -9,4 +9,14 @@
         description = "An API to play whole songs using computer.beep",
         author = "Vexatos"
   }
+    ["test-package"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+	},
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  }
 }
