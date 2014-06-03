c9f59e5dcc8e00b00344ed236a94c4a4b79b5040
Vexatos <Vexatos@users.noreply.github.com>
2014-06-03 08:58:05 +0200

    Update programs.lua

--- programs.lua
+++ programs.lua
@@ -38,5 +38,175 @@
         name = "Morse API",
         description = "An API to encode a string into morse code",
         author = "Vexatos"
+  },
+  ["yet-another-package2"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["yet-another-package3"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["yet-another-package4"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["yet-another-package5"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["yet-another-package6"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["too-many-packages"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["too-many-packages2"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["too-many-packages3"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["too-many-packages4"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["too-many-packages5"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["too-many-packages6"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["too-many-packages7"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["too-many-packages8"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["too-many-packages9"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["too-many-packagesA"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["too-many-packagesB"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
+  },
+  ["too-many-packagesC"] = {
+    files = {
+      ["master/morse"] = "/"
+    },
+    dependencies = {
+    },
+        name = "Morse API",
+        description = "An API to encode a string into morse code",
+        author = "Vexatos"
   }
 }
