bfd0cc84f05242bc894859f150f72b5ce1c76011
Vexatos <Vexatos@users.noreply.github.com>
2014-06-05 21:41:47 +0200

    Derp.

--- op-manager/oppm.lua
+++ op-manager/oppm.lua
@@ -276,7 +276,7 @@ local function installPackage(pack,path,update)
     end
     local success = pcall(downloadFile,"https://raw.githubusercontent.com/OpenPrograms/"..repo.."/"..i,fs.concat(path,j,string.gsub(i,".+(/.-)$","%1"),nil))
     if success then
-      table.insert(tPacks[pack],fs.concat(path,j,string.gsub(i,".+(/.-)$","%1")))
+      table.insert(tPacks[pack],fs.concat(path,j,string.gsub(i,".+(/.-)$","%1"),nil))
     end
   end
   term.write("Done.\nInstalling Dependencies...")
@@ -284,7 +284,7 @@ local function installPackage(pack,path,update)
     if string.lower(string.sub(i,1,4))=="http" then
       local success = pcall(downloadFile,i,fs.concat(path,j,string.gsub(i,".+(/.-)$","%1"),nil))
       if success then
-        table.insert(tPacks[pack],fs.concat(path,j,string.gsub(i,".+(/.-)$","%1")))
+        table.insert(tPacks[pack],fs.concat(path,j,string.gsub(i,".+(/.-)$","%1"),nil))
       end
     else
       local depInfo = getInformation(string.lower(i))
