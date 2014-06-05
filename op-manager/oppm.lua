26afee7efeba7f10087a4930dc8333347a1fccb3
Vexatos <Vexatos@users.noreply.github.com>
2014-06-05 21:23:31 +0200

    Yet another f

--- op-manager/oppm.lua
+++ op-manager/oppm.lua
@@ -181,7 +181,7 @@ local function provideInfo(pack)
 end
 
 local function readFromFile()
-  local path = fs.path(shell.resolve(process.running)).."opdata.svd"
+  local path = fs.path(shell.resolve(process.running())).."opdata.svd"
   local file,msg = io.open(path,"rb")
   if not file then
     io.stderr:write("Error while trying to read package names: "..msg)
@@ -193,7 +193,7 @@ local function readFromFile()
 end
 
 local function saveToFile(tPacks)
-  local file,msg = io.open(fs.path(shell.resolve(process.running)).."opdata.svd","wb")
+  local file,msg = io.open(fs.path(shell.resolve(process.running())).."opdata.svd","wb")
   if not file then
     io.stderr:write("Error while trying to save package names: "..msg)
     return
