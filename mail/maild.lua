03b90ed650c1816fdb84499f5ad53a34d3a2c038
Vexatos <Vexatos@users.noreply.github.com>
2014-07-11 20:06:19 +0200

    Started working on mail program.

---
+++ mail/maild.lua
@@ -0,0 +1,20 @@
+local fs = require("filsystem")
+local component = require("component")
+local process = require("process")
+
+if not component.isAvailable("modem") then
+  io.stderr:write("This program requires a modem to run.\n")
+  return
+end
+local modem = component.modem
+
+local tunnel = nil
+if component.isAvailable("tunnel") then
+  tunnel = component.tunnel
+end
+
+local datpath = fs.concat(fs.path(shell.resolve(process.running())),"/data")
+if not fs.exists(datpath) or not fs.isDirectory() then
+  fs.remove(datpath)
+  fs.makeDirectory(datpath)
+end
