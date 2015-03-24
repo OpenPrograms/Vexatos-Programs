ae52f5f6cd869320a9ddfe6d389536f915c1aa32
Vexatos <Vexatos@users.noreply.github.com>
2015-03-24 19:44:30 +0100

    Update oppm.lua

--- op-manager/oppm.lua
+++ op-manager/oppm.lua
@@ -236,7 +236,7 @@ local function parseFolders(pack, repo, info)
         local newPath = v["download_url"]:gsub("https?://raw.githubusercontent.com/"..repo.."(.+)$", "%1"):gsub("/?$",""):gsub("^/?","")
         tFiles[newPath] = fs.concat(relPath, newPath:gsub(branch.."/(.+)$","%1"), nil)
       elseif v["type"] == "dir" then
-        local newFiles = unserializeFiles(getFolderTable(repo, namePath.."/"..v["path"], branch), repo, branch, fs.concat(relPath, v["path"])
+        local newFiles = unserializeFiles(getFolderTable(repo, namePath.."/"..v["name"], branch), repo, branch, fs.concat(relPath, v["name"])
         for p,q in pairs(newFiles) do
           tFiles[p] = q
         end
