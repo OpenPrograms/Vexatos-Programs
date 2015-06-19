442671321f9bb76825a247c1c9c64d5eec1bd73c
Vexatos <Stuarzt@gmx.de>
2015-06-19 16:49:19 +0200

    As recommended by Magik

--- selene/Plan9k/lib/modules/base/17_selene.lua
+++ selene/Plan9k/lib/modules/base/17_selene.lua
@@ -1,11 +1,13 @@
-local u = kernel.userspace
-if not u._selene then u._selene = {} end
-u._selene.liveMode = dofile("/etc/selene.cfg")
-if u._selene.liveMode then
-  u._PROMPT = _PROMPT
-end
+function start()
+  local u = kernel.userspace
+  if not u._selene then u._selene = {} end
+  u._selene.liveMode = dofile("/etc/selene.cfg")
+  if u._selene.liveMode then
+    u._PROMPT = _PROMPT
+  end
 
-u._selene.initDone = true
-local selene = u.require("selene")
-u._selene.initDone = false
-selene.load(u)
+  u._selene.initDone = true
+  local selene = u.require("selene")
+  u._selene.initDone = false
+  selene.load(u)
+end
