4d6a9172cfaf15d9dc38ed6493c67f05c7b574d9
Vexatos <Stuarzt@gmx.de>
2015-07-10 15:26:51 +0200

    Added -b option to bigreactors-control to make steam-producing reactors more efficient

--- reactor/bigreactors-control.lua
+++ reactor/bigreactors-control.lua
@@ -7,9 +7,13 @@ Usage:
   Optional arguments are turnOn and turnOff, allowing you to specify when to turn the reactor on and when to turn it off. Default values are 0.1 and 0.9
   If you have turbines connected to the computer and the reactor is in steam-producing mode, it will automatically detect that.
   In turbine mode, it will try to keep the turbines at a certain speed.
-    bigreactors-control [-s] [desiredSpeed [, acceptedSpeed] ]
+    bigreactors-control [-s] [-b] [desiredSpeed [, acceptedSpeed] ]
   desiredSpeed then allows you to set the desired rotations per minute. it will default to 1790.
   acceptedSpeed is the amount the turbine's rotation speed may vary from the desired speed before the program starts reacting. Defaults to 50.
+  -b will make the program run the reactor at 100% for a few seconds to find out how much it can produce at most and will then set the control rods to
+  make the reactor only produce as much steam as needed. Make sure to extract all steam from the reactor while the evaluation is running,
+  otherwise it might not find the best possible value! If it is not possible to extract all steam, you should consider running the program without -b
+  and manually set the control rods.
 Author: Vexatos
 ]]
 
@@ -27,6 +31,8 @@ local maxEnergyTurbine = 1000000
 local desiredSpeed = 1790
 --The amount the turbine's rotation speed may vary from the desired speed before the program starts reacting
 local acceptedSpeed = 50
+--The amount of steam one turbine can take per tick, in milibuckets
+local neededSteam = 2000
 
 
 --Code you probably won't need to change starts here
@@ -40,7 +46,7 @@ local term = require("term")
 --This is true if there is no available screen or the option -s is used
 local silent = not term.isAvailable()
 
-local hasCustomValues = false
+local hasCustomValues, shouldChangeRods = false, false
 
 local function serror(msg, msg2)
   msg2 = msg2 or msg
@@ -56,6 +62,7 @@ do
   local shell = require("shell")
   local args, options = shell.parse(...)
   if options.s then silent = true end
+  if options.b then shouldChangeRods = true end
   if #args > 0 then
     turnOn = tonumber(args[1])
     turnOff = tonumber(args[2])
@@ -81,6 +88,7 @@ if reactor.isActivelyCooled() then
     table.insert(turbines, component.proxy(addr))
   end
   maxEnergy = maxEnergyTurbine * #turbines
+  neededSteam = neededSteam * #turbines
   if hasCustomValues then
     desiredSpeed = turnOn or desiredSpeed
     acceptedSpeed = turnOff or acceptedSpeed
@@ -97,11 +105,15 @@ local function fancyNumber(n)
 end
 
 --Displays numbers with a special offset
-local function offset(num, d)
+local function offset(num, d, ext)
   if num == nil then return "" end
   if type(num) ~= "string" then
     if type(num) == "number" then
-      return offset(tostring(math.floor(num)), d)
+      if ext then
+        return offset(tostring(math.floor(num * 100) / 100), d)
+      else
+        return offset(tostring(math.floor(num)), d)
+      end
     end
     return offset(tostring(num), d)
   end
@@ -159,6 +171,8 @@ local function handleReactor()
   end
 end
 
+local benchmark, madeSteamMax = 0, 0
+
 local function handleTurbines()
   local stored, production, engagedCoils, shutPorts = 0, 0, 0, 0
   local rotations = {}
@@ -187,9 +201,10 @@ local function handleTurbines()
         shouldReactorRun = true
       end
     end
-    if speed < (desiredSpeed - acceptedSpeed) and turbine.getInductorEngaged() then
-      turbine.setInductorEngaged(false)
-    else
+    if turbine.getInductorEngaged() then
+      if speed < (desiredSpeed - acceptedSpeed) then
+        turbine.setInductorEngaged(false)
+      end
       engagedCoils = engagedCoils + 1
     end
     if speed > desiredSpeed then
@@ -205,6 +220,34 @@ local function handleTurbines()
     reactor.setActive(false)
   end
   
+  local madeSteam = reactor.getHotFluidProducedLastTick()
+  local neededPercent = 100
+
+  if shouldChangeRods then
+    if benchmark >= 0 then
+      if not reactor.getActive() then
+        reactor.setActive(true)
+      else
+        reactor.setAllControlRodLevels(0)
+        if madeSteam - madeSteamMax < 5 and madeSteam - madeSteamMax > -5 then
+          if benchmark >= 10 then
+            benchmark = -1
+          else
+            benchmark = benchmark + 1
+          end
+        else
+          benchmark = 0
+          madeSteamMax = madeSteam
+        end
+      end
+    else
+      if reactor.getActive() then
+        neededPercent = math.ceil((neededSteam / madeSteamMax) * 100)
+        reactor.setAllControlRodLevels(math.min(math.max(100 - neededPercent, 0), 100))
+      end
+    end
+  end
+
   if not silent then
     term.setCursor(1, y)
     term.clearLine()
@@ -220,7 +263,21 @@ local function handleTurbines()
     term.clearLine()
     term.write("Stored percentage:  " .. offset(stored / maxEnergy * 100, offs) .. " %\n", false)
     term.clearLine()
-    term.write("Current Production: " .. offset(fancyNumber(production), offs) .. " RF/t\n", false)
+    term.write("Energy Production:  " .. offset(fancyNumber(production), offs) .. " RF/t\n", false)
+    term.clearLine()
+    term.write("Fuel Consumption:   " .. offset(reactor.getFuelConsumedLastTick(), offs, true) .. " mB/t\n", false)
+    if shouldChangeRods then
+      term.clearLine()
+      local evl = ""
+      if benchmark >= 0 then
+        evl = " (Evaluating)" .. string.rep(".", benchmark)
+      end
+      term.write("Reactor power:      " .. offset(neededPercent, offs) .. " %" .. evl .. "\n", false)
+    end
+    term.clearLine()
+    term.write("Steam production:   " .. offset(fancyNumber(madeSteam), offs) .. " mB/t\n", false)
+    term.clearLine()
+    term.write("Steam consumption:  " .. offset(fancyNumber(neededSteam), offs) .. " mB/t\n", false)
     term.clearLine()
     term.write("Engaged Coils:      " .. offset(engagedCoils, offs) .. "\n", false)
     term.clearLine()
@@ -256,7 +313,7 @@ while true do
     end
     reactor.setActive(false)
     for _, turbine in ipairs(turbines) do
-      turbine.setFluidFlowRateMax(turbine.turbine.getFluidFlowRateMaxMax())
+      turbine.setFluidFlowRateMax(turbine.getFluidFlowRateMaxMax())
       turbine.setInductorEngaged(true)
     end
     os.exit()
