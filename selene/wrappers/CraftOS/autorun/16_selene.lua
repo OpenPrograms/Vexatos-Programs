802c8c65ab0d844f36c0f789614157f5070b55c1
Vexatos <Stuarzt@gmx.de>
2015-08-05 17:52:01 +0200

    Finished ComputerCraft wrapper for Selene

---
+++ selene/wrappers/CraftOS/autorun/16_selene.lua
@@ -0,0 +1,55 @@
+--This will make Selene work with ComputerCraft.
+
+local function mrpairs(t, oldf, mf)
+  if type(t) == "table" then
+    local res, mt = pcall(getmetatable, t)
+    if res and mt and mt[mf] then
+      local m1,m2,m3 = mt[mf](t)
+      return m1,m2,m3
+    end
+  end
+  return oldf(t)
+end
+
+local function injectConfig()
+  local config = [[
+-- Set this to false to disable live interpreting of Selene files. Will require compiling manually using selene.parse
+local liveMode = false
+
+return liveMode
+]]
+  local path = "/etc/selene.cfg"
+  if not fs.exists(path) then
+    local file = fs.open(path, "w")
+    if file then
+      file.write(config)
+      file.close()
+    end
+  end
+end
+
+local function readConfig()
+  local success, mode = pcall(dofile, "/etc/selene.cfg")
+  if success then
+    _G._selene.liveMode = mode
+  else
+    _G._selene.liveMode = false
+  end
+end
+
+local function load()
+  if not _G._selene then _G._selene = {} end
+  _G._selene._oldpairs = _G.pairs
+  _G._selene._oldipairs = _G.ipairs
+  _G.pairs = function(t)
+    return mrpairs(t, _G._selene._oldpairs, "__pairs")
+  end
+  _G.ipairs = function(t)
+    return mrpairs(t, _G._selene._oldipairs, "__ipairs")
+  end
+  injectConfig()
+  readConfig()
+end
+
+load()
+require("selene")
